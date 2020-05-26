99faa79fd18db7b30c3b952b4e61662ed09b8ae9
Hasatori <hradil.o@email.cz>
2020-05-26 21:15:40 +0200

    Event test added.

--- src/test/java/cz/vse/selenium/churchcrm/EventTest.java
+++ src/test/java/cz/vse/selenium/churchcrm/EventTest.java
@@ -5,8 +5,11 @@ import cz.vse.selenium.churchcrm.testframework.model.EventType;
 import cz.vse.selenium.churchcrm.testframework.page.AddEditChurchEventPage;
 import cz.vse.selenium.churchcrm.testframework.page.ChurchEventsPage;
 import cz.vse.selenium.churchcrm.testframework.page.LoginPage;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import java.time.Month;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,22 +28,25 @@ public class EventTest extends AChurchCrmTest {
         EventType eventType = EventType.Church_Service;
         UUID eventTitle = UUID.randomUUID();
         String eventDescription = "Event description";
+        Integer year = 2021;
+        Month month = Month.APRIL;
+        String from = String.format("%d-%d-27 10:30 PM", year, month.getValue());
+        String eventSermon = "test";
         Boolean active = false;
-        addEditChurchEventPage.createNewEvent(eventType, eventTitle.toString(), eventDescription, active);
-
-
+        addEditChurchEventPage.createNewEvent(eventType, eventTitle.toString(), eventDescription, from, eventSermon, active);
         ChurchEventsPage churchEventsPage = new ChurchEventsPage(driver);
-        GridRow firstRowValues = churchEventsPage.getChurchEventsGrid().search(eventTitle.toString()).get(0);
+        GridRow firstRowValues = churchEventsPage.getChurchEventsGrid(eventType, year, month).search(eventTitle.toString()).get(0);
 
         assertEquals(new ChurchEventsPage(driver).getUrl(), driver.getCurrentUrl());
 
         churchEventsPage.goToEdit(firstRowValues);
+
         assertAll(
                 () -> assertSame(eventType, addEditChurchEventPage.getEventType()),
                 () -> assertEquals(eventTitle.toString(), addEditChurchEventPage.getEventTitle()),
                 () -> assertEquals(eventDescription, addEditChurchEventPage.getEventDescription()),
+                () -> assertEquals(eventSermon, addEditChurchEventPage.getEventSermon()),
                 () -> assertEquals(active, addEditChurchEventPage.getEventStatus())
-
         );
 
     }
